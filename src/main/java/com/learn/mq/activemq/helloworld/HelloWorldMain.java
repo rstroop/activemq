e441704c454b7ebe7d8dcb0f84713faa856a8954
Richard Stroop <rstroop@redhat.com>
2015-10-29 16:17:57 -0500

    slowed down the producer

--- src/main/java/com/learn/mq/activemq/helloworld/HelloWorldMain.java
+++ src/main/java/com/learn/mq/activemq/helloworld/HelloWorldMain.java
@@ -7,32 +7,8 @@ public class HelloWorldMain {
  
     public static void main(String[] args) throws Exception {
         thread(new HelloWorldProducer(), false);
-        thread(new HelloWorldProducer(), false);
+        Thread.sleep(3000);
         thread(new HelloWorldConsumer(), false);
-        Thread.sleep(1000);
-        thread(new HelloWorldConsumer(), false);
-        thread(new HelloWorldProducer(), false);
-        thread(new HelloWorldConsumer(), false);
-        thread(new HelloWorldProducer(), false);
-        Thread.sleep(1000);
-        thread(new HelloWorldConsumer(), false);
-        thread(new HelloWorldProducer(), false);
-        thread(new HelloWorldConsumer(), false);
-        thread(new HelloWorldConsumer(), false);
-        thread(new HelloWorldProducer(), false);
-        thread(new HelloWorldProducer(), false);
-        Thread.sleep(1000);
-        thread(new HelloWorldProducer(), false);
-        thread(new HelloWorldConsumer(), false);
-        thread(new HelloWorldConsumer(), false);
-        thread(new HelloWorldProducer(), false);
-        thread(new HelloWorldConsumer(), false);
-        thread(new HelloWorldProducer(), false);
-        thread(new HelloWorldConsumer(), false);
-        thread(new HelloWorldProducer(), false);
-        thread(new HelloWorldConsumer(), false);
-        thread(new HelloWorldConsumer(), false);
-        thread(new HelloWorldProducer(), false);
     }
  
     public static void thread(Runnable runnable, boolean daemon) {
